27ead0ff6eea710b737bc150e8a5116d8ea85e5e
Tomasz Kolodziej <tomasz.kolodziej@poczta.pl>
2017-03-18 12:21:20 +0100

    testy

--- src/main/java/com/tolean/smssender/EnableSmsSender.java
+++ src/main/java/com/tolean/smssender/EnableSmsSender.java
@@ -1,8 +1,15 @@
 package com.tolean.smssender;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by Tomasz Kołodziej
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface EnableSmsSender {
 
 }
--- src/main/java/com/tolean/smssender/SmsSenderAutoConfigure.java
+++ src/main/java/com/tolean/smssender/SmsSenderAutoConfigure.java
@@ -5,6 +5,7 @@ import com.tolean.smssender.providerConfiguration.SmsapiProviderConfiguration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +20,7 @@ import java.util.concurrent.Executor;
  * Created by Tomasz Kołodziej
  */
 @Configuration
-@ConditionalOnBean({SmsSender.class})
+@ConditionalOnBean(annotation = EnableSmsSender.class)
 @EnableAsync
 @EnableConfigurationProperties({GlobalConfiguration.class, SmsapiProviderConfiguration.class})
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -29,6 +30,7 @@ public class SmsSenderAutoConfigure {
     private final SmsapiProviderConfiguration smsapiProviderConfiguration;
 
     @Bean
+    @ConditionalOnMissingBean
     public SmsSender smsSender() throws ClientException {
         Optional<ProviderConfiguration> providerConfigurationOptional = getProviderConfiguration();
 
